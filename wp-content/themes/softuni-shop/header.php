cad0d0431857412848e9ed0bb76145611b5c624d
Svetos49 <svetivslav@gmail.com>
2023-05-14 11:15:21 +0300

    Theme: add header and footer files and enqueue style

---
+++ wp-content/themes/softuni-shop/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Online Shop</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+
+	<!-- <link rel="stylesheet" href="http://localhost/www/SoftUni/wordPressCours/property/wp-content\themes\softuni-shop\assets\css\master.css"> -->
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+
+    <?php wp_head(); ?>
+</head>
+<body>
+	<div class="site-wrapper">
+		<header class="site-header">
+			<h1 class="site-title"><a href="#">Online Shop</a></h1>
+		</header>
